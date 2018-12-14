f107c89952d37201feca406cf42fd37b77553732
Edward Moon <emoon9323@gmail.com>
2018-12-14 03:16:25 -0600

    Add files via upload

---
+++ Menu.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SleighRush
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.enterBtn = new System.Windows.Forms.Button();
+            this.Exitbtn = new System.Windows.Forms.Button();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.Gold;
+            this.title.Location = new System.Drawing.Point(12, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(294, 55);
+            this.title.TabIndex = 0;
+            this.title.Text = "Sleigh Rush";
+            // 
+            // enterBtn
+            // 
+            this.enterBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.enterBtn.BackColor = System.Drawing.Color.Transparent;
+            this.enterBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterBtn.Location = new System.Drawing.Point(624, 127);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(75, 40);
+            this.enterBtn.TabIndex = 1;
+            this.enterBtn.Text = "Enter";
+            this.enterBtn.UseVisualStyleBackColor = false;
+            this.enterBtn.Click += new System.EventHandler(this.enterBtn_Click);
+            // 
+            // Exitbtn
+            // 
+            this.Exitbtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Exitbtn.BackColor = System.Drawing.Color.Transparent;
+            this.Exitbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exitbtn.Location = new System.Drawing.Point(624, 183);
+            this.Exitbtn.Name = "Exitbtn";
+            this.Exitbtn.Size = new System.Drawing.Size(75, 37);
+            this.Exitbtn.TabIndex = 2;
+            this.Exitbtn.Text = "Exit";
+            this.Exitbtn.UseVisualStyleBackColor = false;
+            this.Exitbtn.Click += new System.EventHandler(this.Exitbtn_Click);
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.BackColor = System.Drawing.Color.Transparent;
+            this.nameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLbl.ForeColor = System.Drawing.Color.Gold;
+            this.nameLbl.Location = new System.Drawing.Point(438, 74);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(277, 24);
+            this.nameLbl.TabIndex = 3;
+            this.nameLbl.Text = "PLEASE ENTER IN A NAME";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(496, 101);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(203, 20);
+            this.inputBox.TabIndex = 4;
+            this.inputBox.TextChanged += new System.EventHandler(this.inputBox_TextChanged);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SleighRush.Properties.Resources.santa_t1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(711, 489);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.Exitbtn);
+            this.Controls.Add(this.enterBtn);
+            this.Controls.Add(this.title);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button enterBtn;
+        private System.Windows.Forms.Button Exitbtn;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.TextBox inputBox;
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace SleighRush
+{
+    public partial class Menu : Form 
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Exitbtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void enterBtn_Click(object sender, EventArgs e)
+        {
+            //Test for user input, if text box left blank unable to enter in game!
+            string text = inputBox.Text;
+            if (text == "")
+            {
+                MessageBox.Show("Name is required!");
+            }
+            else if(text != null)
+            {
+                MessageBox.Show(text,"Welcome");
+                GameScreen gs = new GameScreen();
+                gs.Show();
+            }
+           // this.Hide();
+
+        }
+
+        private void inputBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
